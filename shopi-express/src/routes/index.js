ac09ae7845d89d1e14563496730d449205615c51
Javier Diaz <hey@coderdiaz.me>
2021-04-17 14:11:20 -0500

    feat: added logic to show users api routes

--- shopi-express/src/routes/index.js
+++ shopi-express/src/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 // Add the required routes
 router.use('/auth', require('./auth'));
+router.use('/users', require('./users'));
 
 module.exports = router;
--- shopi-express/src/routes/users.js
+++ shopi-express/src/routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const sequelize = require('../db');
 
 // Get all users
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const users = await sequelize.models.users.findAll();
+
   return res.json({
-    data: [],
+    data: users,
   });
 });
 
